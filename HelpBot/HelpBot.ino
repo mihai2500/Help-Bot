361dbcc04779327aefd8732633c5efd5c6b24b39
mihai2500 <116178013+mihai2500@users.noreply.github.com>
2024-05-14 19:33:22 +0300

    First camera stream ip: 192.168.42.75;

--- HelpBot/HelpBot.ino
+++ HelpBot/HelpBot.ino
@@ -6,6 +6,11 @@ SoftwareWire Wire(sda, scl);
 
 #include <hd44780.h>
 #include <hd44780ioClass/hd44780_I2Cexp.h>
+#include <DHT_Async.h>
+
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+static const int DHT_SENSOR_PIN = 13;
+DHT_Async dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
 hd44780_I2Cexp lcd;
 int row = 1;
@@ -34,6 +39,8 @@ byte colPins[COLS] = {5, 4, 3, 2};
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
+float t;
+
 void setup(){
   Serial.begin(9600);
   int istatus = lcd.begin(16,2);
@@ -48,7 +55,10 @@ void setup(){
   
 void loop(){
   inputKey();
+  readTemp();
+  Serial.println(analogRead(A0));
 
+  
 }
 
  void inputKey(){
@@ -59,19 +69,21 @@ void loop(){
     if(key == confirmKey){
       if(count == 9){
         Serial.println();
-        phoneNumber = number + "/r";
+        phoneNumber = number + '\0';
         Serial.println(phoneNumber);
-        displayPhoneNumber(phoneNumber);
         validPhoneNumber = true;
+        displayPhoneNumber(validPhoneNumber);
       }
       else{
         Serial.println("Invalid phone number");
         lcd.clear();
+        lcd.print("Invalid phone number");
         delay(500);
           for(int i = 0; i <= 9; i++){
             number[i] = ' ';
         }
-        displayNumberInput();
+        validPhoneNumber = false;
+        displayPhoneNumber(validPhoneNumber);
         count = 0;
       }
     }
@@ -89,18 +101,18 @@ void loop(){
             number[i] = ' ';
         }
         count = 0;
-        displayNumberInput();
+        validPhoneNumber = false;
+        displayPhoneNumber(validPhoneNumber);
       }
     }
     else if(key == 'A'){
-      if(validPhoneNumber)
-        displayPhoneNumber(phoneNumber);
-      else
-        displayNumberInput();
+      displayPhoneNumber(validPhoneNumber);
     }
     else if(key == 'B'){
       lcd.clear();
       lcd.print("Temperature");
+      lcd.setCursor(0, 1);
+      lcd.print(String(t) + "°C");
     }
     else if(key == 'C'){
       lcd.clear();
@@ -114,23 +126,57 @@ void loop(){
   }
  }
  
- void displayNumberInput(){
-  lcd.clear();
-  lcd.print("Phone Number:");
-  Serial.println("Phone Number");
-  lcd.setCursor(0,1);
-  Serial.println("+40");
-  lcd.print("+40");
-  column = 3;
-  row = 1;
-  lcd.setCursor(column,row);
- }
-
- void displayPhoneNumber(String nr){
+ void displayPhoneNumber(boolean valid){
+  if(valid){
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("My phone nr:");
     lcd.setCursor(0,1);
-    lcd.print("+40" + nr);
-    Serial.println("+40" + nr);
+    lcd.print("+40" + phoneNumber);
+    Serial.println("+40" + phoneNumber);
+  }
+  else{
+    lcd.clear();
+    lcd.print("Phone Number:");
+    Serial.println("Phone Number");
+    lcd.setCursor(0,1);
+    Serial.println("+40");
+    lcd.print("+40");
+    column = 3;
+    row = 1;
+    lcd.setCursor(column,row);
+  }
+  
  }
+
+static bool measure_environment(float *temperature, float *humidity) {
+    static unsigned long measurement_timestamp = millis();
+
+    /* Measure once every four seconds. */
+    if (millis() - measurement_timestamp > 200ul) {
+        if (dht_sensor.measure(temperature, humidity)) {
+            measurement_timestamp = millis();
+            return (true);
+        }
+    }
+
+    return (false);
+}
+
+void readTemp(){
+  float temperature;
+    float humidity;
+
+    /* Measure temperature and humidity.  If the functions returns
+       true, then a measurement is available. */
+    if (measure_environment(&temperature, &humidity)) {
+        Serial.print("T = ");
+        Serial.print(temperature, 1);
+        t = temperature;
+        Serial.print(" deg. C, H = ");
+        Serial.print(humidity, 1);
+        Serial.println("%");
+    }
+}
+
+
